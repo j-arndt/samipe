0a4561dd30aa9209b9062774b2d9208f32b7f798
Justin Arndt <justinarndt05@gmail.com>
2026-07-06 18:05:39 -0400

    Add impl/__init__.py

---
+++ impl/__init__.py
@@ -0,0 +1,3 @@
+# Copyright (c) 2026 Justin Arndt. All rights reserved.
+# Licensed under the GNU GPLv3. For commercial licensing and proprietary
+# hardware mapping, see the LICENSE file (dual-licensing notice at top).
